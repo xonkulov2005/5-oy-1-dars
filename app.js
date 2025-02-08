4ccde3e3dad8a070245dffc39f883ee92ffa7fb8
muhammadjon <muhammadjon@gmail.com>
2025-02-08 21:55:08 +0500

    Add initial implementation of getAverageAge function and basic HTML structure

---
+++ app.js
@@ -0,0 +1,9 @@
+// Quyidagi massivdagi barcha odamlarni yoshlarining o’rtacha qiymatini chiqaruvchi getAverageAge(arr) nomli funksiya yozing.
+// Input: const people = [
+//   { name: "Abdulaziz", age: 33 },
+//   { name: "Erkin", age: 22 },
+//   { name: "Temur", age: 34 },
+//   { name: "Sardor", age: 20 },
+// ];
+// Output: 27.25
+
